9dad5184a59094ac6ad656fa9577457477e656bc
Steffen Gebert <steffen.gebert@typo3.org>
2016-02-13 10:45:40 +0100

    Extract ssh key as attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,4 @@
 default['backuppc']['user'] = 'backuppc'
+
+#<> The ssh pubkey placed on the clients, which allows the server to login
+default['backuppc']['user_ssh_pubkeys'] = []
--- recipes/default.rb
+++ recipes/default.rb
@@ -21,10 +21,7 @@
 user_account node['backuppc']['user'] do
   username node['backuppc']['user']
   comment "BackupPC user"
-  ssh_keys [
-    "command=\"sudo $SSH_ORIGINAL_COMMAND\" ssh-dss AAAAB3NzaC1kc3MAAACBAOPVf4NG1jtLd4842teaVOa1hsjkBzbpTxpEoJHMHVKqyirRB4fuCFRWxoCdn+Mto78XVc7F7rNz+1ZiHPEWGJIhfnbbpC9X2+zK9ArihGusyT+oGDsN7mr3IikyUTrR3N58Nq2caRoih7BzM6AIDzJ8D8h42m026J9llSzF5r5dAAAAFQCuFqc4K8zYMwnpgmvkdLDnidnTpQAAAIEA1mLKcg5oLZ3u1W7w9Z2wcF0FpEAKMj+ZNG/MbxowTq7LmL5HTn9Zz8nDRWEwYDIEEt3Nx3xhkbjKEqO+jGjdl5HxRYorsXdOQng1iLHkpG2FyFZg8cac0yyggGhVDGmvc/F0PI91a2TpKBvbQUfMlEDuVQhbki8o62N3zBpx4y4AAACAArWnTOQ8gySaC0EcvGLy9I93PhxSPtyEtfJzO7IHo7XdM7vOFLHEGJeCsLklbfbgVAux+/ByZiLd2cOxUA2yqfw4zU4w1D3BKNCtyEfMmxGIWN9eeyN+JtWLv4kO6qp5bYx0XRVJLLJ4+QmJELPaItSdR/8mPZXuu+th7SRAZso= old-key@mstucki",
-    "command=\"sudo $SSH_ORIGINAL_COMMAND\" ssh-dss AAAAB3NzaC1kc3MAAACBAIV5OCUoJjsYLZ5kN/hEmUkcrJw/sLS+VTXg7N0HBVNChwgZEW/uizYbEcl/OWi0HplVYfG3WX1u8X6wWhajrxrTizMaNGoVvUXur/s/SNEW6fg6JKPrIswV0NuWyrwPmzO9582KAa7WU7sBIO0FpiadcU+Srz+eUUap1dYH2eHvAAAAFQCjn7rZdpaoqIbbQJIo2ZlygVt+JwAAAIBcIosaN7RR3Ytc4Fx58do18tSlsFqoQuiafh6iof73hPP/cEwWQRDgXzfmcmkgZne4ijc39TcZAinOJNAO4Em4H4BuPsKyjLj1ST6slvGWtHxyXxrca9yeNSMnOdn2ZhQU7caFqb1nee1m6UlpuBvHz5MShYCpvpAyDtNjxXbg0QAAAIAf6RUuCpR4/uCMtL5mJ53HKBlmVHmF2RemjpcOetb+xGTZHkG9ikMePNQTn23mpLW+ZguI6TIpBkeYUyPj0OT6im+PgOaN7iQOngCINCGnC9+aMKnCIvk3lLs77fcNYKhKZOLZm6O3ODhKkBKN9GJNCVB/qOEjhd5mECJrSWytEA== backuppc@backup01.typo3.org-2012-10-30T08:19:12+0100"
-  ]
+  ssh_keys node['backuppc']['user_ssh_pubkeys']
   ssh_keygen false
 end
 
