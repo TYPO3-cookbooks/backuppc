70567bfafd59c5cb7a2550d970609294058b2292
Michael Stucki <michael.stucki@typo3.org>
2017-07-18 15:29:58 +0200

    [TASK] Update version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          "Apache 2.0"
 description      "BackupPC Server and Client"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 
-version          "1.0.1"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends          "ssl_certificates"
 
